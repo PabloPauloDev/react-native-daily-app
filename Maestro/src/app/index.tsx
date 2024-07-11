d6b84a5a12ed2066d3da4386821bcdc94ae136fa
Pablo Carpanedo <pablopaulodev@gmail.com>
2024-07-11 20:27:42 -0300

    Reestruturando projeto...

---
+++ Maestro/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+const Home = () => {
+    return (
+        <View className=''>
+            <Text>Teste</Text>
+        </View>
+    )
+}
+
+export default Home;
---
+++ Maestro/src/styles/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    white: '#FFFFF',
+    
+    black: {
+        200: '#333333',
+        500: '#000000'
+    }
+}
